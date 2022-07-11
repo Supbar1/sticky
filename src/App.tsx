a5fd1981743a98b46e5eb09168fc29169d57533a
Bartosz Kukier <supbar@wp.pl>
2022-07-11 10:08:55 +0200

    initial commit, api works fine

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+// Components
+import Item from "./item/item";
+// Styles
+import { Wrapper } from "./App.styles";
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
+const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(data);
+
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount, 0);
+
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // 1. Is the item already added in the cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      // First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
+
+  if (error) return <div>Something went wrong ...</div>;
+  console.log(getProducts());
+  return (
+    <Wrapper>
+      {data?.map((item) => (
+        <Item item={item} handleAddToCart={handleAddToCart} />
+      ))}
+    </Wrapper>
+  );
+};
+
+export default App;
---
+++ src/item/item.tsx
@@ -0,0 +1,20 @@
+import { CartItemType } from "../App";
+import { Wrapper } from "./item.styles";
+
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+};
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+  <Wrapper>
+    <img src={item.image} alt={item.title} />
+    <div>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      <h3>${item.price}</h3>
+    </div>
+    <button onClick={() => handleAddToCart(item)}>Add to cart</button>
+  </Wrapper>
+);
+export default Item;
